2968a5c8d79c60ea2ad857f33b3ebd63b24d7d1e
David Stein <dstein131@gmail.com>
2022-12-15 12:29:49 -0500

    ugh

--- masterVerify.js
+++ masterVerify.js
@@ -158,17 +158,115 @@ document.getElementById("reviewBtn").addEventListener("click", function() {
     masterFieldsVerify = {
 
         countyName: document.getElementById("countyName").value,
+        countyCode: document.getElementById("countyCode").value,
+        countyContact: document.getElementById("countyContact").value,
+        countyEmail: document.getElementById("countyEmail").value,
+        countyPhone: document.getElementById("countyPhone").value,
+        countyFax: document.getElementById("countyFax").value,
         countyAddress: document.getElementById("countyAddress").value,
         countyCity: document.getElementById("countyCity").value,
         countyState: document.getElementById("countyState").value,
         countyZip: document.getElementById("countyZip").value,
-        countyPhone: document.getElementById("countyPhone").value,
-        countyFax: document.getElementById("countyFax").value,
-        countyEmail: document.getElementById("countyEmail").value
+        audName: document.getElementById("audName").value,
+        audPhone: document.getElementById("audPhone").value,
+        audContName: document.getElementById("audContName").value,
+        audContPhone: document.getElementById("audContPhone").value,
+        audContEmail: document.getElementById("audContEmail").value,
+        treasName: document.getElementById("treasName").value,
+        treasPhone: document.getElementById("treasPhone").value,
+        treasEmail: document.getElementById("treasEmail").value,
+        countAtty: document.getElementById("countAtty").value,
+        countAttyPhone: document.getElementById("countAttyPhone").value,
+        assessor: document.getElementById("assessor").value,
+        assessorPhone: document.getElementById("assessorPhone").value,
+        countyClerk: document.getElementById("countyClerk").value,
+        countyClerkPhone: document.getElementById("countyClerkPhone").value,
+        commiss1: document.getElementById("commiss1").value,
+        commiss2: document.getElementById("commiss2").value,
+        commiss3: document.getElementById("commiss3").value,
+        commissionPhone: document.getElementById("commissionPhone").value,
+        remindEmail: document.getElementById("remindEmail").value,
+        docEmail: document.getElementById("docEmail").value,
+        signature: countSig
+
     }
+
+    // add the object masterFieldsVerify to the local storage
+    localStorage.setItem("masterFieldsVerify", JSON.stringify(masterFieldsVerify));
+
+    // add the values of the object masterFieldsVerify to the dom id reviewModal
+    document.getElementById("reviewModal").innerHTML = `
+  
+      
+        <h5>County Information</h5>
+        <p>County Name: ${masterFieldsVerify.countyName}</p>
+        <p>County Code: ${masterFieldsVerify.countyCode}</p>
+        <p>County Contact: ${masterFieldsVerify.countyContact}</p>
+        <p>County Email: ${masterFieldsVerify.countyEmail}</p>
+        <p>County Phone: ${masterFieldsVerify.countyPhone}</p>
+        <p>County Fax: ${masterFieldsVerify.countyFax}</p>
+        <p>County Address: ${masterFieldsVerify.countyAddress}</p>
+        <p>County City: ${masterFieldsVerify.countyCity}</p>
+        <p>County State: ${masterFieldsVerify.countyState}</p>
+        <p>County Zip: ${masterFieldsVerify.countyZip}</p>
+       
+        <h5>Auditor Information</h5>
+        <p>Auditor Name: ${masterFieldsVerify.audName}</p>
+        <p>Auditor Phone: ${masterFieldsVerify.audPhone}</p>
+        <p>Auditor Contact Name: ${masterFieldsVerify.audContName}</p>
+        <p>Auditor Contact Phone: ${masterFieldsVerify.audContPhone}</p>
+        <p>Auditor Contact Email: ${masterFieldsVerify.audContEmail}</p>
+      
+        <h5>Treasurer Information</h5>
+        <p>Treasurer Name: ${masterFieldsVerify.treasName}</p>
+        <p>Treasurer Phone: ${masterFieldsVerify.treasPhone}</p>
+        <p>Treasurer Email: ${masterFieldsVerify.treasEmail}</p>
+     
+
+        <h5>County Attorney Information</h5>
+            
+        <p>County Attorney: ${masterFieldsVerify.countAtty}</p>
+        <p>County Attorney Phone: ${masterFieldsVerify.countAttyPhone}</p>
+       
+        <h5>Assessor Information</h5>
+        <p>Assessor: ${masterFieldsVerify.assessor}</p>
+        <p>Assessor Phone: ${masterFieldsVerify.assessorPhone}</p>
+      >
+        <h5>County Clerk Information</h5>
+        <p>County Clerk: ${masterFieldsVerify.countyClerk}</p>
+        <p>County Clerk Phone: ${masterFieldsVerify.countyClerkPhone}</p>
+     
+     
+        <h5>Commissioner Information</h5>
+        <p>Commissioner 1: ${masterFieldsVerify.commiss1}</p>
+        <p>Commissioner 2: ${masterFieldsVerify.commiss2}</p>
+        <p>Commissioner 3: ${masterFieldsVerify.commiss3}</p>
+        <p>Commissioner Phone: ${masterFieldsVerify.commissionPhone}</p>
+    
+        <h5>Reminder Email</h5>
+        <p>Reminder Email: ${masterFieldsVerify.remindEmail}</p>
+        <h5>Document Email</h5>
+        <p>Document Email: ${masterFieldsVerify.docEmail}</p>
+        <h5>Signature</h5>
+        <p>County Official Signature: <img src="${masterFieldsVerify.signature}" alt="signature"></p>
+        `
+
 
     // console log the object masterFieldsVerify
     console.log(masterFieldsVerify);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 );
