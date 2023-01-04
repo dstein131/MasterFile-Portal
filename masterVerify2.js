5883527964337edafb3794ead1d16af76fd42e38
David Stein <dstein131@gmail.com>
2023-01-04 17:58:11 -0500

    size h2

--- masterVerify2.js
+++ masterVerify2.js
@@ -705,23 +705,39 @@ document.getElementById("addnewsBtn").addEventListener("click", () => {
 )
 
 // make an array of different light background colors
-let lightColors = ["#f8f9fa", "#e9ecef", "#dee2e6", "#faf9f2", "#f2f7f3", "#e6eff7", "#f2edf5", "#f2f2eb", "#e8ebea"];
+let lightColors = ["#f8f9fa", "#eefff8", "#e9ecef", "#ffeeee", "#fff5ee", "#dee2e6", "#eefcff", "#e6eff7", "#f2edf5", "#f2f2eb", "#e8ebea", "#fcffee", "#fffaee"];
 
 
 
-function changeColor() {
-  // loop through all the elements with the class name of nav-link and create an array of them
-  let navLinks = document.getElementsByClassName("nav-link");
-  // for each navLink add an event listener 
-  for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", () => {
-      // set a background random color from the lightColors array to the dom id cardBody
-      document.getElementById("cardBody").style.backgroundColor = lightColors[Math.floor(Math.random() * lightColors.length)];
-    })
-  }
-}
+// function changeColor() {
+//   // loop through all the elements with the class name of nav-link and create an array of them
+//   let navLinks = document.getElementsByClassName("nav-link");
+//   // for each navLink add an event listener 
+//   for (let i = 0; i < navLinks.length; i++) {
+//     navLinks[i].addEventListener("click", () => {
+//       // set a background random color from the lightColors array to the dom id cardBody
+//       document.getElementById("cardBody").style.backgroundColor = lightColors[Math.floor(Math.random() * lightColors.length)];
+//     })
+//   }
+// }
 
-changeColor();
+// changeColor();
 
 
+// // asign a different color from the array light colors as bs-data-color to each nav-link
+// let navLinks = document.getElementsByClassName("nav-link");
+// for (let i = 0; i < navLinks.length; i++) {
+//   navLinks[i].setAttribute("bs-data-color", lightColors[i]);
+// }
 
+// function backgroundChange() {
+//  // add an event listener to each of the nav-links that changes the background color of the dom id cardBody to the bs-data-color of the nav-link
+//   let navLinks = document.getElementsByClassName("nav-link");
+//   for (let i = 0; i < navLinks.length; i++) {
+//     navLinks[i].addEventListener("click", () => {
+//       document.getElementById("cardBody").style.backgroundColor = navLinks[i].getAttribute("bs-data-color");
+//     })
+//   }
+// }
+
+// backgroundChange();
